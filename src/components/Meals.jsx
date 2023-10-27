d38de0ae4aaf2deb3c7392f60822aca259366d70
dzoliana <dzolicana@gmail.com>
2023-10-27 15:51:04 +0200

    Initial commit

---
+++ src/components/Meals.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const responsse = await fetch("http://localhost:3000/meals");
+
+      if (!responsse.ok) {
+        //...
+      }
+      const meals = await responsse.json();
+      setLoadedMeals(meals);
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
